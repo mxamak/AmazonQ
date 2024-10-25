21e7a837bcf5f767f191476b27369ab8408e1876
Mohammed Aqib <157542380+mxamak@users.noreply.github.com>
2024-10-25 09:46:52 -0500

    Update samplePythonScript.py

--- samplePythonScript.py
+++ samplePythonScript.py
@@ -6,4 +6,4 @@ root = tk.Tk()
 root.withdraw()  # Hide the root window
 
 # Show the message box with "Hello, world!"
-messagebox.showinfo("Greeting", "Hello, world!")
+messagebox.showinfo("Greeting", "This script has been downloaded from Github and executed. It can be found in your downloads folder.")
